90d460815d3082f9e25e242f73f8c671239f3531
Ljupcho Apostolov <lja@rackbeat.com>
2021-12-02 13:50:08 +0100

    move methods from models to builder classes

--- src/Builders/InvoiceBuilder.php
+++ src/Builders/InvoiceBuilder.php
@@ -3,9 +3,48 @@
 namespace LasseRafn\Dinero\Builders;
 
 use LasseRafn\Dinero\Models\Invoice;
+use LasseRafn\Dinero\Requests\PaymentRequestBuilder;
 
 class InvoiceBuilder extends Builder
 {
     protected $entity = 'invoices';
     protected $model = Invoice::class;
+
+    /**
+     * @param $id
+     *
+     * @return PaymentRequestBuilder
+     */
+    public function payments($id) {
+        return new PaymentRequestBuilder( new PaymentBuilder( $this->request, "invoices/{$id}/payments" ) );
+    }
+
+    /**
+     * @param $orgId
+     * @param $id
+     * @param $timestamp
+     *
+     * @return mixed
+     */
+    public function book($orgId, $id, $timestamp)
+    {
+        return $this->request->curl->post('https://api.dinero.dk/v1/' . $orgId . '/invoices/' . $id . '/book', [
+            'json' => [
+                'Timestamp' => $timestamp,
+            ]
+        ]);
+    }
+
+    /**
+     * Send invoice to customer by email @todo Add possible email parameters from https://api.dinero.dk/openapi/index.html#tag/Invoices/paths/~1v1~1{organizationId}~1invoices~1{guid}~1email/post
+     *
+     * @param $orgId
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function send($orgId, $id)
+    {
+        return $this->request->curl->post('https://api.dinero.dk/v1/' . $orgId . '/invoices/' . $id . '/email');
+    }
 }
--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -2,13 +2,11 @@
 
 namespace LasseRafn\Dinero\Models;
 
-use LasseRafn\Dinero\Builders\PaymentBuilder;
-use LasseRafn\Dinero\Requests\PaymentRequestBuilder;
 use LasseRafn\Dinero\Utils\Model;
 
 class Invoice extends Model
 {
-	protected $entity     = 'invoices';
+	protected $entity = 'invoices';
 	public $primaryKey = 'Guid';
 
 	public $PaymentDate;
@@ -49,37 +47,4 @@ class Invoice extends Model
 	public $ProductLines;
 
 	public $Address;
-
-    /**
-     * @return PaymentRequestBuilder
-     */
-	public function payments() {
-		return new PaymentRequestBuilder( new PaymentBuilder( $this->request, "invoices/{$this->Guid}/payments" ) );
-	}
-
-    /**
-     * @param $orgId
-     * @return mixed
-     */
-	public function book($orgId) {
-
-	    return $this->request->curl->post('https://api.dinero.dk/v1/' . $orgId . '/invoices/' . $this->{$this->primaryKey} . '/book', [
-
-	        'json' => [
-
-	            'Timestamp' => $this->{'TimeStamp'},
-            ]
-        ]);
-    }
-
-    /**
-     * Send invoice to customer by email @todo Add possible email parameters from https://api.dinero.dk/openapi/index.html#tag/Invoices/paths/~1v1~1{organizationId}~1invoices~1{guid}~1email/post
-     *
-     * @param $orgId
-     * @return mixed
-     */
-    public function send($orgId)
-    {
-        return $this->request->curl->post('https://api.dinero.dk/v1/' . $orgId . '/invoices/' . $this->{$this->primaryKey} . '/email');
-    }
 }
